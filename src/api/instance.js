bc13709183b2b0ec00a0504224817f6051ff1e6c
Abdullah <abdullahvora136@gmail.com>
2025-02-23 02:53:13 +0530

    ..

--- src/api/instance.js
+++ src/api/instance.js
@@ -31,7 +31,7 @@
 import axios from "axios";
 
 const apiInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://webdefender-backend.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
